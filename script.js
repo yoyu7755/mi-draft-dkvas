53f1efafa4989f8959f1c8bb06a079391fca67ca
yoyu7755 <juan_rubiitoo@hotmail.com>
2026-09-17 17:43:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ btnIniciar.addEventListener("click", () => {
     obtenerSiguienteCandidato();
 });
 
-// audioSilbato.play().catch(error => console.log("Permiso de audio requerido:", error));
+
 function iniciarTemporizador() {
     // Limpiar cualquier conteo previo activo
     clearInterval(intervaloTiempo);
